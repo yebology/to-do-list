5b583536b852fe24aeacd131fc68530d83e53e27
yebology <yobelnathaniel12@gmail.com>
2024-06-18 20:34:35 +0700

    connect api without db

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"github.com/gofiber/fiber/v2"
+) 
 
 func main() {
 	fmt.Println("Hello, World!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
