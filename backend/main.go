560008255af7784e99487f9e5d07e005b0261761
yebology <yobelnathaniel12@gmail.com>
2024-06-20 05:09:20 +0700

    debugging error authentication failed mongodb

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,139 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 ) 
+
+// omitempty biar kalo id ga dikasi, bakal diisi string acak dari mongoDB
+type Todo struct {
+	ID 			primitive.ObjectID 		`json:"id, omitempty" bson:"_id, omitempty"`
+	Completed 	bool 					`json:"completed"`
+	Body 		string 					`json:"body"`
+}
+
+var collection *mongo.Collection
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Disconnect(context.Background()) // disconnect dari database setelah main selesai dipanggil
+	
+	err = client.Ping(context.Background(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to Atlas MongoDB")
+
+	collection = client.Database("golang_db").Collection("todos")
+
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
-	})
+	app.Get("/api/todos", getTodos)
+	app.Post("/api/todos", createTodos)
+	app.Patch("/api/todos/:id", updateTodo)
+	// app.Patch("/api/todos/:id", deleteTodo)
 
-	log.Fatal(app.Listen(":4000"))
+	port:= os.Getenv("PORT")
+
+	if port == "" {
+		port = "6000"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
+}
+
+func getTodos(c *fiber.Ctx) error {
+	var todos []Todo
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		return err
+	}
+
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var todo Todo
+		if err := cursor.Decode(&todo); err != nil {
+			return err
+		}
+		todos = append(todos, todo)
+	}
+
+	return c.JSON(todos)
+}
+
+
+func createTodos(c *fiber.Ctx) error {
+	todo := new(Todo)
+
+	// bodyparse itu mengambil data dari pengguna dan konversi ke format yang mudah dipahami oleh server
+	if err := c.BodyParser(todo); err != nil {
+		return err
+	}
+
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error":"Todo body cannot be empty"})
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), todo)
+
+	if err != nil {
+		return err
+	}
+
+	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
+
+	return c.Status(201).JSON(todo)
+}
+
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error":"Invalid todo ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+
+	update := bson.M{
+		"$set": bson.M{
+			"completed": bson.M{"$not": bson.M{"$eq": true}},
+		},
+	}
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success":"true"})
 }
